a2972ddc7e0802a013c253959a447708b30e62a6
Jobayer <jobayerhossain977@gmail.com>
2023-09-17 10:00:49 +0600

    staging: Added methods

--- src/websocket.gateway.ts
+++ src/websocket.gateway.ts
@@ -1,4 +1,24 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({})
-export class WebsocketGateway {}
+export class WebsocketGateway {
+  @WebSocketServer() server: Server;
+
+  @SubscribeMessage('sendMessage')
+  handleEvent(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
+    this.server.emit('receiveMessage', {
+      id: client.id,
+      message: data?.message,
+      name: data?.name,
+      time: data?.time,
+    });
+  }
+}
